230a2d76f4ca51c295a32b0c3e4a7028e0decad3
Sami Silén <shan@laukkoski.com>
2016-06-28 14:42:43 +0300

    Fixed routes back, because those actually works

--- config/routes.php
+++ config/routes.php
@@ -2,14 +2,11 @@
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 
-/*Router::plugin(
+Router::plugin(
     'Attribute',
     ['path' => '/attribute'],
     function (RouteBuilder $routes) {
         $routes->fallbacks('DashedRoute');
     }
-);*/
-
-Router::scope('/attribute', ['plugin' => 'Attribute'], function ($routes) {
-      $routes->fallbacks();
+);
 });
