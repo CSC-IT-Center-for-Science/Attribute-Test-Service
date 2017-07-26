6da1e1447d7d90320af648098fbd51f056146375
Sami Silén <sasilen@users.noreply.github.com>
2017-07-26 07:11:59 +0300

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -3,7 +3,7 @@ use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 
 Router::plugin(
-    'cscfi/AttributeTestService',
+    'Cscfi/AttributeTestService',
     ['path' => '/attribute'],
     function (RouteBuilder $routes) {
         $routes->fallbacks('DashedRoute');
